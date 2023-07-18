fc25c5afd914a1b5ec7f26e717bf6c4f61364579
Enzo C <125571218+LunaticBH@users.noreply.github.com>
2023-07-18 10:31:52 -0400

    Update morse.py

--- morse.py
+++ morse.py
@@ -115,12 +115,12 @@ class Solution:
         message = message.strip().upper()
         for i in range(len(message)):
             if message[i] == " ":
-		s += 1
-	    if s = 2:
+                s += 1
+            if s = 2:
 		answer += " "
             else:
                 answer += MORSE_CODE_DICT[message[i]] + " "
-		s = 0
+                s = 0
         return answer           
 
 def main():
